1b0f687b2d208e9df1fe1e122e59182563b83602
1213816214 <wer1213816214@qq.com>
2026-06-02 14:37:27 +0800

    Add files via upload
    计算BMI值

---
+++ BMIjianpanluru.java
@@ -0,0 +1,15 @@
+package com.lianxi;
+import java.util.Scanner;
+public class BMIjianpanluru {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入体重(单位千克):");
+        int a = sc.nextInt();
+        System.out.println( a );
+        System.out.println("请输入身高(单位米):");
+        double b = sc.nextDouble();
+        System.out.println( b );
+        System.out.println("BMI指数为：");
+        System.out.println(a/(b*b));
+    }
+}
